45291de883c3d9800c9c50b3e30e8b36556ffe3f
Caitlan Krasinski <60022257+caitlan-krasinski@users.noreply.github.com>
2022-07-31 15:51:14 -0400

    Delete .cache/joblib/statsbombpy/helpers/pluralize directory

--- .cache/joblib/statsbombpy/helpers/pluralize/func_code.py
+++
@@ -1,9 +0,0 @@
-# first line: 58
-@memory.cache
-def pluralize(word):
-    word = engine.plural(word)
-    word = word.replace("*", "")
-    word = word.replace("-", "_")
-    word = word.replace(" ", "_")
-    word = word.lower()
-    return word
